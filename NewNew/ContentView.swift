8462c0649cafbb507c570dd555dc811d66617bf1
Sean van der Wal <sean@Seans-Pro.local>
2019-10-17 16:05:45 -0700

    newnew

---
+++ NewNew/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NewNew
+//
+//  Created by Sean van der Wal on 10/17/19.
+//  Copyright © 2019 Sean van der Wal. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ NewNewTests/NewNewTests.swift
@@ -0,0 +1,34 @@
+//
+//  NewNewTests.swift
+//  NewNewTests
+//
+//  Created by Sean van der Wal on 10/17/19.
+//  Copyright © 2019 Sean van der Wal. All rights reserved.
+//
+
+import XCTest
+@testable import NewNew
+
+class NewNewTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
